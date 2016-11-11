7e7a19a28306dd4e49166499ef9a3a434be46723
nikymorg <nicole.morgan@gmail.com>
2016-11-11 15:34:18 -0500

    Rep API established' '

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function callAPI(address){
   $.ajax({
     method: 'GET',
-    url: `https://www.googleapis.com/civicinfo/v2/representatives?address=10001&key=AIzaSyDk5sk3q7VI2FSxfSR2lZw8V-pduLf0N_A`
+    url: `https://www.googleapis.com/civicinfo/v2/representatives?address=10001&key=APIKEY`
   }).done(function(response) {
     console.log(response)
   })
