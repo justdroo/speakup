e248a1c541412ea870530fdf5622de69b892496c
nikymorg <nicole.morgan@gmail.com>
2016-11-13 13:20:47 -0500

    Added handlebars, created form

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
-function callAPI(address){
-  $.ajax({
-    method: 'GET',
-    url: `https://www.googleapis.com/civicinfo/v2/representatives?address=10001&key=APIKEY`
-  }).done(function(response) {
-    console.log(response)
+function representativesAdapter(){
+  const address = $("#address").val()
+  $.get(`https://www.googleapis.com/civicinfo/v2/representatives?address=${address}&key=${APIKey}`,
+  data => {
+    const template = Handlebars.compile($('#results-template').html())
+    $('#results').html(template(data))
+  }).fail(error => {
+    displayError()
   })
 }
 
-callAPI('10001')
+function displayError() {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+// function searchRepositories() {
+//   const searchTerms = $('#searchTerms').val()
+//   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, data => {
+//       const template = Handlebars.compile($('#results-template').html())
+//       $('#results').html(template(data))
+//     }).fail(error => {
+//       displayError()
+//     })
+// }
+
+
+// $(function(){
+//   $('form').on('submit', function(event){
+//     event.preventDefault()
+//     let address = $('#address').val()
+//     representativesAdapter(address).done(function() {
+//       $('#results').html = (`<img src='${artist.imageUrl}' />`)
+//     })
+//   })
+// }
