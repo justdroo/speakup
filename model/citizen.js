13a997fefebbcf2a5a7c997b686d111eddbcee02
Flatiron School <flatironschool@Drew-2.local>
2016-11-14 10:37:01 -0500

    Drew's final commit from weekend (index.html borken)

--- model/citizen.js
+++
@@ -1,7 +0,0 @@
-class Citizen {
-  constructor(zip, issue, stance) {
-    this.zip = zip;
-    this.issue = issue;
-    this.stance = stance;
-  }
-}
--- model/representative.js
+++
@@ -1,9 +0,0 @@
-class Representative {
-  constructor(name) {
-    this.name = name;
-    this.party = party;
-    this.position = position;
-    this.email = email;
-    this.twitter = twitter;
-  }
-}
